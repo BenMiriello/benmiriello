d1a119028bc421ecd8151af926faf5e2ae983056
BenMiriello <benmiriello@gmail.com>
2020-11-25 11:33:09 -0500

    Add image files, grabameeting collate

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './styles/tailwind.output.css';
+// import doodle from './doodle.svg'
+import grabAMeetingCollage from './assets/grabameetingcollage.png'
 
 const App = () => {
   return (
-    <div className='w-screen h-screen bg-gray-800'>
+    <div style={{backgroundColor: '#333'}} className='w-screen h-screen'>
+      <div className='w-full h-24'>
+        <div className='font-bold text-2xl text-white p-2 flex items-center h-full'>BEN MIRIELLO</div>
+      </div>
+      <div style={{height: '36rem'}} className='mx-4 mb-4 bg-gray-700'>
+        <img src={grabAMeetingCollage} alt='grab a meeting interface collage' />
+      </div>
+      <div style={{height: '36rem'}} className='mx-4 mb-4 bg-gray-700'></div>
       
-      <div className='text-center text-white'>Ben Miriello</div>
+      {/* <img className='absolute w-full opacity-50 mx-auto' src={doodle} alt='doodle' /> */}
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    // fontFamily: {
+    //   'body': ['Vollkorn'],
+    //   'sans': ['Vollkorn'],
+    //   'serif': ['Lato'],
+    //   'lato': ['Lato', 'sans-serif'],
+    //   'vollkorn': ['Vollkorn', 'serif'] ,
+    // },
+    fontFamily: {
+      serif: "Vollkorn",
+      sans: [ "Lato",],
+      archivo: ["'Archivo Black'"],
+    },
   },
   variants: {
     extend: {},
