2579a4ee1fe8d00628d5a39a592827090fa2527d
BenMiriello <benmiriello@gmail.com>
2020-11-25 17:38:45 -0500

    Put text over my face

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,25 @@ import benMiriello from './assets/benmiriello.jpeg'
 
 const App = () => {
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full relative'>
       <div className='w-full h-24'>
         <div className='font-bold font-oswald font-sans text-4xl p-4 flex items-center h-full'>BEN MIRIELLO</div>
       </div>
 
-      <div style={{height: '42rem', backgroundColor: '#222'}} className='m-4 flex justify-end'>
-        <div style={{width: '26rem', minHeight: '40rem', backgroundImage: `url(${benMiriello})`}} className='max-w-3xl overflow-hidden bg-cover bg-no-repeat'>
-          <div className='w-full h-full bg-opacity-30 bg-black p-8 sm:p-12'></div>
+      <div style={{height: '42rem'}} className='m-4 flex justify-between'>
+        <div className='flex flex-col justify-center'>
+          <h1 className='text-6xl z-20 mr-24 sm:ml-12 sm:mr-64 md:ml-24 md:mr-72'>Ben is a web developer who loves bringing ideas to life</h1>
         </div>
+        <div style={{width: '30rem', minHeight: '40rem', maxWidth: '90vw'}} className='z-10 absolute right-0 mx-4 float-right max-w-3xl bg-opacity-30 bg-black'></div>
+        <div style={{width: '30rem', minHeight: '40rem', maxWidth: '90vw', backgroundImage: `url(${benMiriello})`}} className='absolute right-0 mx-4 float-right max-w-3xl overflow-hidden bg-cover bg-no-repeat'></div>
       </div>
 
       <div style={{height: '48rem', backgroundImage: `url(${grabAMeetingCollage})`, backgroundPosition: 'center'}} className='mx-4 mb-4 overflow-hidden bg-cover bg-no-repeat'>
         <div className='w-full h-full bg-opacity-75 bg-black p-8 sm:p-12'>
           <div className='h-24 sm:h-16 mb-12 sm:mb-24 sm:flex sm:items-center'>
-            <div className='flex'>
-              <img className='h-12 mb-2' src={grabAMeetingGWhite} alt='g icon for grab a meeting' />
-              <p className='font=lato text-3xl ml-4 pt-2'>Grab&nbsp;a&nbsp;Meeting</p>
+            <div className='flex items-center'>
+              <img className='h-12' src={grabAMeetingGWhite} alt='g icon for grab a meeting' />
+              <p className='font=lato text-3xl ml-6'>Grab&nbsp;a&nbsp;Meeting</p>
             </div>
             {/* <div className='sm:ml-6 sm:text-base md:text-lg text-gray-400 flex '>a&nbsp;place&nbsp;for&nbsp;people&nbsp;in&nbsp;recovery&nbsp;to&nbsp;connect</div> */}
           </div>
