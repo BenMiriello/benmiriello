b3a7b10d2408b59131f6f253cbaaa60432fd0388
BenMiriello <benmiriello@gmail.com>
2020-11-25 12:18:01 -0500

    Add full grabameeting logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './styles/tailwind.output.css';
-// import doodle from './doodle.svg'
 import grabAMeetingCollage from './assets/grabameetingcollage.png'
-import grabAMeetingGWhite from './assets/grabameeting-g-white.png'
+import grabAMeetingWhite from './assets/grabameeting-white.png'
 
 const App = () => {
   return (
@@ -14,15 +13,13 @@ const App = () => {
       <div style={{height: '36rem', backgroundImage: `url(${grabAMeetingCollage})`, backgroundPosition: 'center'}} className='mx-4 mb-4 overflow-hidden bg-cover bg-no-repeat'>
         <div className='w-full h-full bg-opacity-70 bg-black p-4'>
           <div className='h-16 flex flex-no-wrap items-center'>
-            <img className='h-10 px-4' src={grabAMeetingGWhite} />
-            <div className='text-white text-2xl font-bold'>Grab a Meeting</div>
+            <img className='h-12' src={grabAMeetingWhite} />
+            {/* <div className='text-white text-2xl font-bold'>Grab a Meeting</div> */}
           </div>
         </div>
       </div>
 
       <div style={{height: '36rem'}} className='mx-4 mb-4 bg-gray-700'></div>
-      
-      {/* <img className='absolute w-full opacity-50 mx-auto' src={doodle} alt='doodle' /> */}
     </div>
   )
 }
